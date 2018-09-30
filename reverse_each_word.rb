126a39124eaf95a92dd6d0cdc9ba445b36f01abb
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-30 21:30:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-#def reverse_each_word(sentence)
- # reversed_words = []
- # sentence.split(" ").each do |word|
-  #  reversed_words.push(word.reverse)
- # end
- # reversed_words.join(" ")
-#end
+def reverse_each_word(sentence)
+  reversed_words = []
+  sentence.split(" ").each do |word|
+    reversed_words.push(word.reverse)
+  end
+  reversed_words.join(" ")
+end
 
 def reverse_each_word(sentence)
   reversed = sentence.split(" ").collect do |word|
